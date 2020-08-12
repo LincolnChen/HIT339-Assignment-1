933a9cb853b70b0ceb719c4c8be4f029040766f0
Silin Chen <silin.chen05@gmail.com>
2020-08-12 19:51:01 +0930

    Added Items.cs class

---
+++ Assignment1_Salesboard/Models/Items.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+
+namespace Assignment1_Salesboard.Models
+{
+    public class Items
+    {
+        public int Id { get; set; }
+        public string ItemName { get; set; }
+
+        [Display(Name = "Item Description"), StringLength(255)]
+        public string ItemDescription { get; set; }
+
+        public int Quantity { get; set; }
+        public decimal Price { get; set; }
+    }
+}
